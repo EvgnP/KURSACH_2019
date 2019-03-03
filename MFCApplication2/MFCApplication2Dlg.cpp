be75eb1528ab33c59c7113a42edc5af04bf1bd65
EvgnP <minatounix@gmail.com>
2019-03-03 22:45:10 +0300

    Добавьте файлы проекта.

---
+++ MFCApplication2/MFCApplication2Dlg.cpp
@@ -0,0 +1,166 @@
+
+// MFCApplication2Dlg.cpp : файл реализации
+//
+
+#include "stdafx.h"
+#include "MFCApplication2.h"
+#include "MFCApplication2Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Диалоговое окно CAboutDlg используется для описания сведений о приложении
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // поддержка DDX/DDV
+
+// Реализация
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// диалоговое окно CMFCApplication2Dlg
+
+
+
+CMFCApplication2Dlg::CMFCApplication2Dlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_MFCAPPLICATION2_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication2Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication2Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_EN_CHANGE(IDC_EDIT1, &CMFCApplication2Dlg::OnEnChangeEdit1)
+	ON_BN_CLICKED(IDCANCEL, &CMFCApplication2Dlg::OnBnClickedCancel)
+	ON_BN_CLICKED(IDOK, &CMFCApplication2Dlg::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// обработчики сообщений CMFCApplication2Dlg
+
+void CMFCApplication2Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// При добавлении кнопки свертывания в диалоговое окно нужно воспользоваться приведенным ниже кодом,
+//  чтобы нарисовать значок.  Для приложений MFC, использующих модель документов или представлений,
+//  это автоматически выполняется рабочей областью.
+
+void CMFCApplication2Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // контекст устройства для рисования
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Выравнивание значка по центру клиентского прямоугольника
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Нарисуйте значок
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// Система вызывает эту функцию для получения отображения курсора при перемещении
+//  свернутого окна.
+HCURSOR CMFCApplication2Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCApplication2Dlg::OnEnChangeEdit1()
+{
+	// TODO:  Если это элемент управления RICHEDIT, то элемент управления не будет
+	// send this notification unless you override the CDialogEx::OnInitDialog()
+	// function and call CRichEditCtrl().SetEventMask()
+	// with the ENM_CHANGE flag ORed into the mask.
+
+	// TODO:  Добавьте код элемента управления
+	/*CWnd* pWn1 = GetDlgItem(IDC_STATIC);
+	
+	CWnd* pWn = GetDlgItem(IDC_EDIT1);
+
+	if(pWn) {
+		
+		CString strA;
+
+		pWn->SetFont(&m_Font);
+
+		pWn->GetWindowText(strA);
+
+		m_val = atof(CW2A(strA));
+
+		pWn1->SetWindowText(strA);
+
+	
+	}*/
+}
+
+
+void CMFCApplication2Dlg::OnBnClickedCancel()
+{
+	// TODO: добавьте свой код обработчика уведомлений
+	CDialogEx::OnCancel();
+}
+
+
+void CMFCApplication2Dlg::OnBnClickedOk()
+{
+	// TODO: добавьте свой код обработчика уведомлений
+	CDialogEx::OnOK();
+}
---
+++ MFCApplication2/MFCApplication2Dlg.h
@@ -0,0 +1,142 @@
+
+// MFCApplication2Dlg.h : файл заголовка
+//
+
+#pragma once
+
+
+// диалоговое окно CMFCApplication2Dlg
+class CMFCApplication2Dlg : public CDialogEx
+{
+// Создание
+public:
+	CMFCApplication2Dlg(CWnd* pParent = NULL);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+public:
+
+	CFont m_Font;
+	double m_val;
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog()
+	{
+		CDialogEx::OnInitDialog();
+
+		// Добавление пункта "О программе..." в системное меню.
+
+		// IDM_ABOUTBOX должен быть в пределах системной команды.
+		ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+		ASSERT(IDM_ABOUTBOX < 0xF000);
+
+		CMenu* pSysMenu = GetSystemMenu(FALSE);
+		if (pSysMenu != NULL)
+		{
+			BOOL bNameValid;
+			CString strAboutMenu;
+			bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+			ASSERT(bNameValid);
+			if (!strAboutMenu.IsEmpty())
+			{
+				pSysMenu->AppendMenu(MF_SEPARATOR);
+				pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+			}
+		}
+
+		// Задает значок для этого диалогового окна.  Среда делает это автоматически,
+		//  если главное окно приложения не является диалоговым
+		SetIcon(m_hIcon, TRUE);			// Крупный значок
+		SetIcon(m_hIcon, FALSE);		// Мелкий значок
+
+										//шрифт изменение
+
+		m_Font.CreateFontW(20, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 0, 0, _T("Comic Sans"));
+
+		// TODO: добавьте дополнительную инициализацию
+		//ПРИМЕР ВЗАИМОДЕЙСТВИЯ
+		CWnd* pWn = GetDlgItem(IDC_STATIC_N);
+
+		if (pWn) {
+
+			CString strA;
+
+			pWn->SetFont(&m_Font);
+
+			pWn->GetWindowText(strA);
+
+			strA += _T(":");
+
+			pWn->SetWindowText(strA);
+
+		}
+
+		pWn = GetDlgItem(IDC_STATIC_f);
+
+		if (pWn) {
+
+			CString strA;
+
+			pWn->SetFont(&m_Font);
+
+			pWn->GetWindowText(strA);
+
+			strA += _T(":");
+
+			pWn->SetWindowText(strA);
+
+		}
+
+		pWn = GetDlgItem(IDC_STATIC_a);
+
+		if (pWn) {
+
+			CString strA;
+
+			pWn->SetFont(&m_Font);
+
+			pWn->GetWindowText(strA);
+
+			strA += _T(":");
+
+			pWn->SetWindowText(strA);
+
+		}
+
+		pWn = GetDlgItem(IDC_STATIC_c);
+
+		if (pWn) {
+
+			CString strA;
+
+			pWn->SetFont(&m_Font);
+
+			pWn->GetWindowText(strA);
+
+			strA += _T(":");
+
+			pWn->SetWindowText(strA);
+
+		}
+
+		return TRUE;  // возврат значения TRUE, если фокус не передан элементу управления
+	}
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit1();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnBnClickedOk();
+};
